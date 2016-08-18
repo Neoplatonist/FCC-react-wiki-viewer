5297e07776a73743dade6d91e4260ee9837c7145
Neoplatonist <mr.neoplatonist@gmail.com>
2016-08-18 13:35:13 +0900

    Initializing Repo

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>
+          Created By: <a target="_blank" href="http://joshfjohnston.com">Josh Johnston</a> for <a target="_blank" href="https://www.freecodecamp.com">FCC</a> - <a target="_blank" href="https://www.freecodecamp.com/challenges/build-a-wikipedia-viewer">Challenge</a>
+        </p>
+      </footer>
+    );
+  }
+}
---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper cyan">
+            <a href="" className="brand-logo center">Wikipedia Viewer</a>
+          </div>
+        </nav>
+
+        <div id="notification" className="orange center"></div>
+      </header>
+    );
+  }
+}
